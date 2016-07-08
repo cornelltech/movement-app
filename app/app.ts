99bd55b4fac5be9cc8e4423240c8c7b3f7913f87
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-07-07 22:07:04 -0400

    set status bar to light

--- app/app.ts
+++ app/app.ts
@@ -35,7 +35,7 @@ export class MyApp {
 
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
+      StatusBar.styleLightContent();
 
    
       // BackgroundGeolocation is highly configurable. See platform specific configuration options
