6ff9182d38ed554a2a6c28e2f61dec2589ec248d
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-07-18 17:51:01 -0400

    added a list of revealed users + revealed pie chart

--- app/pages/venue-list/venue-revealed-users.ts
+++ app/pages/venue-list/venue-revealed-users.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {ViewController, NavParams} from 'ionic-angular';
+import {CHART_DIRECTIVES} from 'ng2-charts/ng2-charts';
 import {Venue} from '../../models/venue';
 
 // <ion-item *ngFor="let participant in venue.revealed_users">
@@ -15,25 +16,48 @@ import {Venue} from '../../models/venue';
         <ion-icon name="close"></ion-icon>
       </button>
     </ion-buttons>
-
-    
   </ion-toolbar>
 
-  <ion-content>
+  <ion-content style="margin-top:44px;">
+    
+    <base-chart class="chart"
+                  [data]="chartData"
+                  [labels]="chartLabels"
+                  [chartType]="chartType"
+                  [options]="chartOptions"></base-chart>
+    
     <ion-list>
-      
+      <ion-list-header>
+        Revealed Users
+      </ion-list-header>
+      <ion-item *ngFor="let user of venue.revealed_users">
+        {{user}}
+      </ion-item>
     </ion-list>
-  </ion-content>`
+  </ion-content>`,
+  directives: [CHART_DIRECTIVES]
 })
 export class RevealedUserListModal {
   venue:Venue;
+  chartType:string = 'pie';
+  chartLabels:string[] =[];
+  chartData:number[] = [];
+  chartOptions:any = {
+    animation: false,
+    responsive: false,
+  };
+  dataLoaded:boolean = false
 
   constructor(private viewCtrl: ViewController,
              public params: NavParams) {
                  console.log(params.data.venue);
                  this.venue = params.data.venue;
 
+                 this.chartLabels = ['Revealed', 'Visited'];
+                 this.chartData = [this.venue.reveals, this.venue.checkins];
   }
+
+  
 
   close() {
     this.viewCtrl.dismiss();
