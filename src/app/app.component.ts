d3cc392a36d618abd43cc9fdca4cfc6478c3b483
AVatch <avatchinsky@outlook.com>
2017-09-02 00:30:07 -0400

    hooked up some of the services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,48 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {Storage } from '@ionic/storage';
 
 import { HomePage } from '../pages/home/home';
+
+import {AuthService} from './../services/auth';
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, 
+              statusBar: StatusBar, 
+              splashScreen: SplashScreen, 
+              private authService:AuthService,
+              private storage:Storage) {
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.authService.loadToken();
+      this.routeToRootPage();
+
     });
+  }
+  
+  routeToRootPage(){
+    this.storage.get('token').then(t => {
+      let authenticated = t ? true:false;
+        if(authenticated){
+          // this.rootPage = TabsPage;
+          console.log('authenticated');
+        }else{
+          // this.rootPage = WelcomePage;
+          console.log('not authenticated');
+        }
+      });
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -8,6 +9,13 @@ import { IonicStorageModule } from '@ionic/storage';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import {SettingsService} from './../services/settings';
+import {AuthService} from './../services/auth';
+import {AccountService} from './../services/account';
+import {VenueService} from './../services/venues';
+import {GeoService} from './../services/geo';
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -15,6 +23,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
     IonicStorageModule.forRoot()
   ],
@@ -26,6 +35,11 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    SettingsService, 
+    AuthService, 
+    AccountService, 
+    VenueService,
+    GeoService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
